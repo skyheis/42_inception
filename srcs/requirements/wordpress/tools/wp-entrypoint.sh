3dca649d2e579a52ec505dac140a455af492ac75
skyheis <giuliogt7@gmail.com>
2023-05-29 11:29:22 +0200

    mon

--- srcs/requirements/wordpress/tools/wp-entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp-entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-service php7.4-fpm start
+# service php7.4-fpm start
+
+wget -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
+
+tar -xzvf /tmp/wordpress.tar.gz -C /tmp
+
+mkdir -p /var/www/html
+
+mv /tmp/wordpress/* /var/www/html
+
+chown -R www-data.www-data /var/www/html
+chmod -R 755 /var/www/html
 
 exec "$@"
