4dadca373f9c7023cffbf376a2a4f93aba13eb5d
skyheis <giuliogt7@gmail.com>
2023-05-30 12:12:24 +0200

    thu

--- srcs/requirements/wordpress/tools/wp-entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp-entrypoint.sh
@@ -6,14 +6,15 @@ if [ -d "/var/www/html/wp-admin" ]; then
 	echo "Wordpress already installed"
 else
 	echo "Wordpress installation"
+	wget -O /tmp/wordpress.tar.gz https://wordpress.org/wordpress-6.1.3.tar.gz
+
+	tar -xzf /tmp/wordpress.tar.gz -C /tmp
+
+	mkdir -p /var/www/html
+
+	mv /tmp/wordpress/* /var/www/html
+
 fi
-wget -O /tmp/wordpress.tar.gz https://wordpress.org/wordpress-6.1.3.tar.gz
-
-tar -xzf /tmp/wordpress.tar.gz -C /tmp
-
-mkdir -p /var/www/html
-
-mv /tmp/wordpress/* /var/www/html
 
 chown -R www-data.www-data /var/www/html
 chmod -R 755 /var/www/html
