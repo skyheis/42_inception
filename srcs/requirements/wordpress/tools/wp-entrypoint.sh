a76b82e736e85e01ecf300a64f6e70453b73367f
skyheis <giuliogt7@gmail.com>
2023-05-30 14:03:01 +0200

    thu

--- srcs/requirements/wordpress/tools/wp-entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+MYSQL_DATABASE=wordpress
+MYSQL_USER=ggiannit
+MYSQL_PASSWORD=culocane
+
 # service php7.4-fpm start
 
 if [ -d "/var/www/html/wp-admin" ]; then
@@ -18,6 +22,22 @@ fi
 
 chown -R www-data.www-data /var/www/html
 chmod -R 755 /var/www/html
+
+if [ -f "/var/www/html/wp-config.php" ]; then
+	echo "Wordpress already configured"
+else
+
+	mv /tmp/uploads /var/www/html/wp-content/uploads
+
+	echo "Wordpress configuration"
+	
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	
+	wp core config --dbhost=ggiannit_mariadb --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD
+	
+	chmod 660 wp-config.php
+fi
+
 
 service php7.4-fpm start
 service php7.4-fpm stop
