fc78b26450ab85295bc08645bebf89a38bb2d39f
skyheis <giuliogt7@gmail.com>
2023-05-31 16:15:51 +0200

    browsh

--- srcs/requirements/wordpress/tools/wp-entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp-entrypoint.sh
@@ -33,13 +33,13 @@ else
 
 	wp --allow-root --path=/var/www/html core config --dbhost=ggiannit_mariadb --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD
 
-	wp config set WP_REDIS_HOST ggiannit_redis --allow-root
-	wp config set WP_REDIS_PORT 6379 --raw --allow-root
-	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-	wp config set WP_REDIS_CLIENT phpredis --allow-root
-	wp plugin install redis-cache --activate --allow-root
-	wp plugin update --all --allow-root
-	wp redis enable --allow-root
+	wp config set WP_REDIS_HOST ggiannit_redis --allow-root --path=/var/www/html
+	wp config set WP_REDIS_PORT 6379 --raw --allow-root --path=/var/www/html
+	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root --path=/var/www/html
+	wp config set WP_REDIS_CLIENT phpredis --allow-root --path=/var/www/html
+	wp plugin install redis-cache --activate --allow-root --path=/var/www/html
+	wp plugin update --all --allow-root --path=/var/www/html
+	wp redis enable --allow-root --path=/var/www/html
 
 	# echo "define('WP_HOME','https://ggiannit.42.fr');" >> /var/www/html/wp-config.php
 	# echo "define('WP_SITEURL','https://ggiannit.42.fr');" >> /var/www/html/wp-config.php
